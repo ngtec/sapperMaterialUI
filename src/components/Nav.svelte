fdbcac78b7f6c688e4a3fd4af413d135780b003e
Nils Gleichmann <nils@dehna.de>
2021-02-23 16:31:27 +0100

    init

---
+++ src/components/Nav.svelte
@@ -0,0 +1,25 @@
+<script>
+	export let segment;
+	import TopAppBar, {Row, Section, Title} from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+  import Checkbox from '@smui/checkbox';
+  import FormField from '@smui/form-field';
+
+  let prominent = false;
+  let dense = false;
+  let secondaryColor = false;
+</script>
+
+<TopAppBar variant="static" {prominent} {dense} color={secondaryColor ? 'secondary' : 'primary'}>
+	<Row>
+	  <Section>
+		<IconButton class="material-icons">menu</IconButton>
+		<Title>Static</Title>
+	  </Section>
+	  <Section align="end" toolbar>
+		<IconButton class="material-icons" aria-label="Download">file_download</IconButton>
+		<IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+		<IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+	  </Section>
+	</Row>
+  </TopAppBar>
